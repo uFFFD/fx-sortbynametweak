6d3166ba1f71632ab191baf2655a96f654d06cde
uFFFD <xefbfbd@gmail.com>
2015-01-17 00:11:48 +0800

    update copyright year

--- content/sbntPlacesUtils.js
+++ content/sbntPlacesUtils.js
@@ -1,5 +1,5 @@
 /*  Sort By Name Tweak
- *  Copyright (c) 2014 uFFFD
+ *  Copyright (c) 2014-2015 uFFFD
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
